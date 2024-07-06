beac416bc0ec8cf2bd4560dab61825bc1d4797c2
Mehmet Said Hüseyinoğlu <114436845+SIYAKS-ARES@users.noreply.github.com>
2024-07-06 20:26:14 +0300

    Hangman

--- 7.Hangman/main.py
+++ 7.Hangman/main.py
@@ -42,6 +42,7 @@ while not end_of_game:
 
     print(stages[lives])
 
+'''
 # TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 # Delete this line: word_list = ["ardvark", "baboon", "camel"] ??
 # TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
@@ -50,6 +51,7 @@ while not end_of_game:
 # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
 # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
 # TODO-2: - Import the stages from hangman_art.py and make this error go away.
+'''
 
 
 
